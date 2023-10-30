fc296b6c9d4f8c74f0d5c34ab18d7fb6e068d52c
Cristiano Ferro De Franca <135919856+CristianoFranca1976@users.noreply.github.com>
2023-10-30 15:18:21 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    let abrirInformacao = document.getElementById("aviso");
+    abrirInformacao.style.display = "flex";
+}, 5000);
+function resetPagina() {
+    location.reload();
+}
